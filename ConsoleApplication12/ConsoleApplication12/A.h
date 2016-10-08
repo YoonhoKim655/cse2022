91584351d8e94c96074b2c93f9d6cf7c03c5e279
maumnamu <maumnamu2@naver.com>
2016-10-08 17:25:51 +0900

    Update A.h

--- ConsoleApplication12/ConsoleApplication12/A.h
+++ ConsoleApplication12/ConsoleApplication12/A.h
@@ -9,7 +9,7 @@ public:
 public:
 	A(const int& _i0, const int& _j0, const int& _i1, const int& _j1, const int& _i2, const int& _j2, const int& _i3, const int& _j3, const int& _i4, const int& _j4, const float& red, const float& green, const float& blue);
 
-	void initialize(const int& _i0, const int& _j0, const int& _i1, const int& _j1, const int& _i2, const int& _j2, const int& _i3, const int& _j3, const int& _i4, const int& _j4);
+	void ini(const int& _i0, const int& _j0, const int& _i1, const int& _j1, const int& _i2, const int& _j2, const int& _i3, const int& _j3, const int& _i4, const int& _j4);
 
 	void draw(float* pixels);
 
