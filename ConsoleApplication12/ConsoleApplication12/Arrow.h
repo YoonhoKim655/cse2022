ce62bc5bd6e94d4527428f361cdbdbda5b0608dc
maumnamu <maumnamu2@naver.com>
2016-10-08 17:27:05 +0900

    Update Arrow.h

--- ConsoleApplication12/ConsoleApplication12/Arrow.h
+++ ConsoleApplication12/ConsoleApplication12/Arrow.h
@@ -9,7 +9,7 @@ public:
 public:
 	Arrow(const int& _i0, const int& _j0, const int& _i1, const int& _j1, const int& _i2, const int& _j2, const int& _i3, const int& _j3, const float& red, const float& green, const float& blue);
 
-	void initialize(const int& _i0, const int& _j0, const int& _i1, const int& _j1, const int& _i2, const int& _j2, const int& _i3, const int& _j3);
+	void ini(const int& _i0, const int& _j0, const int& _i1, const int& _j1, const int& _i2, const int& _j2, const int& _i3, const int& _j3);
 
 	void draw(float* pixels);
 
